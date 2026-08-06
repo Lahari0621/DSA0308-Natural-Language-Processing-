3b90ededbcb60ba16b6228be941bb5c40e17402a
T S Sai Lahari <sailaharithalanki@gmail.com>
2026-08-06 10:32:14 +0530

    Add stemming and lemmatization functionality

---
+++ Lab Programs/EX-3.py
@@ -0,0 +1,16 @@
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+
+text = input("Enter a sentence: ")
+
+words = text.split()
+
+print("\nWord\t\tStem\t\tLemma")
+print("-" * 45)
+
+for word in words:
+    stem = stemmer.stem(word)
+    lemma = lemmatizer.lemmatize(word)
+    print(f"{word:15}{stem:15}{lemma}")
