73f1c1cd502ada66660be5681ba8c6d7f959daea
T S Sai Lahari <sailaharithalanki@gmail.com>
2026-08-06 10:41:27 +0530

    Create EX-7.py

---
+++ Lab Programs/EX-7.py
@@ -0,0 +1,12 @@
+import nltk
+
+text = input("Enter a sentence: ")
+
+words = nltk.word_tokenize(text)
+pos_tags = nltk.pos_tag(words)
+
+print("\nWord\t\tPart of Speech")
+print("-" * 35)
+
+for word, tag in pos_tags:
+    print(f"{word:15}{tag}")
